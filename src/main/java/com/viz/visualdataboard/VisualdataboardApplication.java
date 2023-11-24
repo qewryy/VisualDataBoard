8d2887bf11dee64eff7d74d741760206d01e9a1c
qewryy <qewryy4@gmail.com>
2023-11-25 02:42:35 +0900

    First commit

---
+++ src/main/java/com/viz/visualdataboard/VisualdataboardApplication.java
@@ -0,0 +1,13 @@
+package com.viz.visualdataboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualdataboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualdataboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viz/visualdataboard/VisualdataboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viz.visualdataboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualdataboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
